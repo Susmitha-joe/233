b0bc40a8e0d373666dce6fd2ce708f53054418b8
SUSMITHA .R <97079183+Susmitha-joe@users.noreply.github.com>
2022-01-18 21:15:36 -0800

    product.cs

---
+++ .NET refresher/console app 3/product.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    public class Product : Order
+    {
+        public string GetProductName()
+        {
+            return "IPhone";
+        }
+    }
+}
